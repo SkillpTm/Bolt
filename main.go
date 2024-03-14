d9dc2b1c0667a38db2dcda92403c1eafbcdde0a6
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-14 02:21:09 +0100

    changed some app options

--- main.go
+++ main.go
@@ -17,14 +17,17 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "ModSearch",
-		Width:  1024,
-		Height: 768,
+		Title:             "ModSearch",
+		Width:             571,
+		Height:            46,
+		DisableResize:     true,
+		Frameless:         true,
+		HideWindowOnClose: true,
+		AlwaysOnTop:       true,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
 		},

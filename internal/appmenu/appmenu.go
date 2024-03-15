bca8aeed7dded3071432318868a10053cbeda061
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-15 02:05:14 +0100

    added reload and quit shorcuts

---
+++ internal/appmenu/appmenu.go
@@ -0,0 +1,28 @@
+// Package appmenu ...
+package appmenu
+
+// <---------------------------------------------------------------------------------------------------->
+
+import (
+	"github.com/wailsapp/wails/v2/pkg/menu"
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+
+	"github.com/skillptm/ModSearch/internal/app"
+)
+
+// <---------------------------------------------------------------------------------------------------->
+
+// Get provides our default menu
+func Get(a *app.App) *menu.Menu {
+	AppMenu := menu.NewMenu()
+	FileMenu := AppMenu.AddSubmenu("Shortcuts")
+	FileMenu.AddText("Reload", keys.Combo("s", keys.CmdOrCtrlKey, keys.ShiftKey), func(_ *menu.CallbackData) {
+		runtime.WindowReloadApp(a.CTX)
+	})
+	FileMenu.AddText("Quit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
+		runtime.Quit(a.CTX)
+	})
+
+	return AppMenu
+}

3230d24237775fb4b98f8e9693b130d227a4e5a5
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-22 07:50:46 +0100

    added generateMaxXomponents

--- frontend/src/app/uihandler.ts
+++ frontend/src/app/uihandler.ts
@@ -1,3 +1,7 @@
+export {UIHandler};
+
+/* <----------------------------------------------------------------------------------------------------> */
+
 /*
 A component looks like this in html:
 
@@ -17,4 +21,65 @@ interface Component {
     name: HTMLDivElement;
     seperator: HTMLDivElement;
     value: HTMLSpanElement;
+}
+
+/* <----------------------------------------------------------------------------------------------------> */
+
+// UIHandler updates the UI of the application to the current state
+class UIHandler {
+
+    static TOP_BAR_SIZE = 45;
+    static COMPONENT_SIZE = 40;
+
+    searchBar = document.getElementById("search-bar") as HTMLInputElement;
+
+    components = [] as Array<Component>;
+
+    // constructor adds as many components to the application as specififed. This will be the max amount for dispalying anything.
+    constructor(max: number) {
+        this.#generateMaxComponents(max);
+    }
+
+    // #generateMaxComponents generates interface Components appended to the application
+    #generateMaxComponents(max: number): void {
+        const body = document.body;
+
+        for (let index = 0; index < max; index++) {
+            const newBodyElement = this.#makeElement("div", `component${index+1}`, ["hideComp"]) as HTMLDivElement;
+            const newSubImage = this.#makeElement("img", `component${index+1}-image`, ["compImg"]) as HTMLImageElement;
+            const newTextDiv = this.#makeElement("div", `component${index+1}-text`, ["compText"]) as HTMLDivElement;
+            const newNameDiv = this.#makeElement("div", `component${index+1}-name`, ["compName"]) as HTMLDivElement;
+            const newTextSeperator = this.#makeElement("div", `component${index+1}-seperator`, ["compSep"]) as HTMLDivElement;
+            const newTextSpan = this.#makeElement("span", `component${index+1}-value`, ["compValue"]) as HTMLSpanElement;
+
+            newTextDiv.appendChild(newNameDiv);
+            newTextDiv.appendChild(newTextSeperator);
+            newTextDiv.appendChild(newTextSpan);
+            newBodyElement.appendChild(newSubImage);
+            newBodyElement.appendChild(newTextDiv);
+            body.appendChild(newBodyElement);
+
+            const newComponent: Component = {
+                self: newBodyElement,
+                image: newSubImage,
+                text: newTextDiv,
+                name: newNameDiv,
+                seperator: newTextSeperator,
+                value: newTextSpan,
+            }
+
+            this.components.push(newComponent);
+        }
+    }
+
+    // #makeElement makes an HTML element with an id and classes
+    #makeElement(tagName: string, id: string, classes: Array<string>): HTMLElement {
+        const newElement = document.createElement(tagName);
+        newElement.id = id;
+        classes.forEach(newClass => {
+            newElement.classList.add(newClass);
+        });
+        
+        return newElement;
+    }
 }

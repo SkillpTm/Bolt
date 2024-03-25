faf33b21f3ca4440feb7ebdb7157dbad5230d69a
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-25 04:40:59 +0100

    added app into systray

--- internal/appmenu/appmenu.go
+++ internal/appmenu/appmenu.go
@@ -4,6 +4,7 @@ package appmenu
 // <---------------------------------------------------------------------------------------------------->
 
 import (
+	"github.com/getlantern/systray"
 	"github.com/wailsapp/wails/v2/pkg/menu"
 	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
@@ -13,19 +14,42 @@ import (
 
 // <---------------------------------------------------------------------------------------------------->
 
+var AppIcon []byte
+var AppInstance *app.App
+
+// <---------------------------------------------------------------------------------------------------->
+
 // Get provides our default menu
-func Get(a *app.App) *menu.Menu {
+func Get() *menu.Menu {
 	appMenu := menu.NewMenu()
 	subMenu := appMenu.AddSubmenu("Shortcuts")
-	subMenu.AddText("Quit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
-		runtime.WindowHide(a.CTX)
+	subMenu.AddText("Hide", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
+		runtime.WindowHide(AppInstance.CTX)
 	})
-	subMenu.AddText("Quit", keys.CmdOrCtrl("f"), func(_ *menu.CallbackData) {
-		runtime.EventsEmit(a.CTX, "pageForward")
+	subMenu.AddText("Page forwards", keys.CmdOrCtrl("f"), func(_ *menu.CallbackData) {
+		runtime.EventsEmit(AppInstance.CTX, "pageForward")
 	})
-	subMenu.AddText("Quit", keys.CmdOrCtrl("b"), func(_ *menu.CallbackData) {
-		runtime.EventsEmit(a.CTX, "pageBackward")
+	subMenu.AddText("Page backwards", keys.CmdOrCtrl("b"), func(_ *menu.CallbackData) {
+		runtime.EventsEmit(AppInstance.CTX, "pageBackward")
 	})
 
 	return appMenu
+}
+
+func OnReady() {
+	systray.SetIcon(AppIcon)
+	systray.SetTooltip("Quick Search")
+	open := systray.AddMenuItem("Open", "opens quick search bar")
+	quit := systray.AddMenuItem("Quit", "properly quits quick search")
+
+	// OnReady gets started as a goroutine, so we can have an infinite for loop here
+	for {
+		select {
+		case <-open.ClickedCh:
+			runtime.WindowShow(AppInstance.CTX)
+		case <-quit.ClickedCh:
+			systray.Quit()
+			runtime.Quit(AppInstance.CTX)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"embed"
 	"fmt"
 
+	"github.com/getlantern/systray"
 	"github.com/skillptm/Quick-Search/internal/app"
 	"github.com/skillptm/Quick-Search/internal/appmenu"
 	"github.com/skillptm/bws"
@@ -17,27 +18,38 @@ import (
 
 // <---------------------------------------------------------------------------------------------------->
 
-//go:embed all:frontend/dist
+//go:embed frontend/dist/*
 var assets embed.FS
 
 //go:embed frontend/src/assets/images/*
 var images embed.FS
+
+//go:embed build/windows/icon.ico
+var icon embed.FS
 
 // <---------------------------------------------------------------------------------------------------->
 
 func main() {
 	bws.ForceUpdateCache()
 
-	// Create an instance of the app structure
-	app, err := app.NewApp(images)
+	appInstance, err := app.NewApp(images)
 	if err != nil {
 		fmt.Println("couldn't create new app: ", err.Error())
 		return
 	}
 
-	// Create application with options
+	appmenu.AppIcon, err = icon.ReadFile("build/windows/icon.ico")
+	if err != nil {
+		fmt.Println("couldn't get image build/windows/icon.ico from embed: ", err.Error())
+		return
+	}
+
+	appmenu.AppInstance = appInstance
+
+	go systray.Run(appmenu.OnReady, func() {})
+
 	err = wails.Run(&options.App{
-		Title:             "Quick-Search",
+		Title:             "Quick Search",
 		Width:             570,
 		Height:            45,
 		DisableResize:     true,
@@ -45,14 +57,14 @@ func main() {
 		HideWindowOnClose: true,
 		AlwaysOnTop:       true,
 		StartHidden:       true,
-		Menu:              appmenu.Get(app),
+		Menu:              appmenu.Get(),
 		LogLevel:          logger.INFO,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		OnStartup: app.Startup,
+		OnStartup: appInstance.Startup,
 		Bind: []interface{}{
-			app,
+			appInstance,
 		},
 	})
 

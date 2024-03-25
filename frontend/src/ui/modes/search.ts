e154825250eed64977e9a0c88e2d130c329e5256
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-25 09:16:08 +0100

    added buttons to change the page

--- frontend/src/ui/modes/search.ts
+++ frontend/src/ui/modes/search.ts
@@ -17,20 +17,36 @@ class Search {
     #highlightedComp = 0;
     #results: Array<string> = [];
     #resultPage = 0;
+    #searching = false;
 
     /**
-     * Sets the uiHandler as a property.
+     * Sets the uiHandler as a property. Also adds event listeners for the page buttons to the left and right icons.
      *
      * @param uiHandler the main uiHandler used throught the app
      */
     constructor(uiHandler: UIHandler) {
         this.uiHandler = uiHandler;
+
+        this.uiHandler.leftIcon.addEventListener("mouseenter", () => {this.updateLeftRightIcons(true, true);});
+        this.uiHandler.leftIcon.addEventListener("mouseleave", () => {this.updateLeftRightIcons(false, true);});
+        this.uiHandler.leftIcon.addEventListener("click", () => {
+            this.updatePage(-1);
+            this.updateLeftRightIcons(true, true);
+        });
+
+        this.uiHandler.rightIcon.addEventListener("mouseenter", () => {this.updateLeftRightIcons(true, false);});
+        this.uiHandler.rightIcon.addEventListener("mouseleave", () => {this.updateLeftRightIcons(false, false);});
+        this.uiHandler.rightIcon.addEventListener("click", () => {
+            this.updatePage(1);
+            this.updateLeftRightIcons(true, false);
+        });
     }
 
     /**
      * Removes any image from the right icon and starts the loading animation.
      */
     newSearch(): void {
+        this.#searching = true;
         this.uiHandler.rightIcon.src = "";
 
         this.uiHandler.rightSection.classList.add("loading-grid");
@@ -42,6 +58,7 @@ class Search {
      * @param results the new results we received
      */
     async newResults(results: Array<string>): Promise<void> {
+        this.#searching = false;
         this.#results = results;
 
         this.updatePage(0)
@@ -157,5 +174,53 @@ class Search {
      */
     getHoveredFile(comp: Component): string {
         return this.#results[this.#resultPage * this.uiHandler.components.length + (parseInt(comp.self.id[9]) -1)];
+    }
+
+    /**
+     * Changes the left or right icon to an arrow for pag changes when approriate.
+     *
+     * @param mouseEnter if we just entered or left the icon
+     * 
+     * @param left if we need to change the left or right icon
+     */
+    async updateLeftRightIcons(mouseEnter: boolean, left: boolean): Promise<void> {
+        const resetIcons = async () => {
+            if (left) {
+                this.uiHandler.leftIcon.classList.remove("icon-clickable");
+
+                this.uiHandler.leftIcon.src = await GetImageData("magnifying_glass");
+            } else {
+                this.uiHandler.rightIcon.classList.add("icon-clickable");
+
+                if (this.#results.length > 0) {
+                    this.uiHandler.rightIcon.src = await GetImageData("tick");
+                } else {
+                    this.uiHandler.rightIcon.src = await GetImageData("cross");
+                }
+            }
+        }
+
+        if (this.uiHandler.searchBar.value.length === 0 || this.#searching || !mouseEnter) {
+            await resetIcons();
+            return;
+        }
+
+        if (left) {
+            this.uiHandler.leftIcon.classList.add("icon-clickable");
+
+            if (this.#resultPage > 0) {
+                this.uiHandler.leftIcon.src = await GetImageData("left");
+            } else {
+                this.uiHandler.leftIcon.src = await GetImageData("not-left");
+            }
+        } else {
+            this.uiHandler.rightIcon.classList.add("icon-clickable");
+
+            if (this.#resultPage * this.uiHandler.components.length < this.#results.length - 1) {
+                this.uiHandler.rightIcon.src = await GetImageData("right");
+            } else {
+                this.uiHandler.rightIcon.src = await GetImageData("not-right");
+            }
+        }
     }
 }

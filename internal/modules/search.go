d45784be2b8ce3239d3d73f10eec8919b182d8cc
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-03-31 11:13:24 +0200

    rewrote the interface for the search

--- internal/modules/search.go
+++ internal/modules/search.go
@@ -1,141 +1,55 @@
-// Package modules contains the main Search function and start up logic of bws.
+// Package modules holdes the interfaces for core modules of Bolt
 package modules
 
-// <---------------------------------------------------------------------------------------------------->
-
 import (
-	"log"
+	"fmt"
 	"runtime"
-	"time"
 
 	"github.com/skillptm/Bolt/internal/modules/search"
 	"github.com/skillptm/Bolt/internal/modules/search/cache"
-	"github.com/skillptm/Bolt/internal/modules/search/config"
 )
 
-// <---------------------------------------------------------------------------------------------------->
+// SearchModule is an interface which will hold the indexed cache and be the start point for searches
+type SearchModule struct {
+	fileSystem *cache.Filesystem
+}
 
-// init executes as soon as bws is imported into another project and launches a goroutine of updateCache
-func init() {
-	var err error
-	config.BWSConfig, err = config.New(config.DefaultConfig)
+// NewSearchModule is the constructor for SearchModule, which also sets up the cache and the Filesystem
+func NewSearchModule() (*SearchModule, error) {
+	sm := SearchModule{}
+
+	fs, err := cache.NewFilesystem()
 	if err != nil {
-		log.Fatal(err)
+		return &sm, fmt.Errorf("NewFilesystem: couldn't setup Filesystem:\n--> %w", err)
 	}
 
-	go updateCache()
+	sm.fileSystem = fs
+
+	return &sm, nil
 }
 
-// updateCache updates with the use of tickers both the MainDirs and the SecondaryDirs
-func updateCache() {
-	// create tickers for how often the FileSystem components are supossed to update
-	mainDirsTicker := time.NewTicker(3 * time.Minute)
-	defer mainDirsTicker.Stop()
+// Search is the public facing wrapper for the search function
+func (sm *SearchModule) Search(searchString string, fileExtensions []string, extendedSearch bool, forceStopChan chan bool) []string {
+	return search.Start(searchString, fileExtensions, sm.fileSystem, extendedSearch, forceStopChan)
+}
 
-	secondaryDirsTicker := time.NewTicker(30 * time.Minute)
-	defer secondaryDirsTicker.Stop()
-
-	for {
-		select {
-		case <-mainDirsTicker.C:
-			if !cache.EntrieFilesystem.SetupProperly {
-				continue
-			}
-
-			// check if the Filesystem is Updateable, if so update it, otherwise wait for the next cycle
-			if cache.EntrieFilesystem.Updateable {
-				cache.EntrieFilesystem.Update(config.BWSConfig.MainDirs, true)
-				runtime.GC()
-			}
-		case <-secondaryDirsTicker.C:
-			if !cache.EntrieFilesystem.SetupProperly {
-				continue
-			}
-
-			// check if the Filesystem is Updateable, if so update it, otherwise wait for the next cycle
-			if cache.EntrieFilesystem.Updateable {
-				cache.EntrieFilesystem.Update(config.BWSConfig.SecondaryDirs, false)
-				runtime.GC()
-			}
+// ForceUpdateCache immediately updates the cache. If extended is set the default/extended caches are updated and reset will reset the whole Filesystem on the SearchModule
+func (sm *SearchModule) ForceUpdateCache(extended bool, reset bool) error {
+	if reset {
+		fs, err := cache.NewFilesystem()
+		if err != nil {
+			return fmt.Errorf("ForceUpdateCache: couldn't setup Filesystem:\n--> %w", err)
 		}
-	}
-}
 
-/*
-baseSearch is a wrapper around the search and rank functions and returns the ranked search results at the end.
-
-Additionally if baseSearch was launched by GoSearchWithBreak it can be stopped at any point with the forceStopChan.
-If the search was stopped early we return a true, otherwise false.
-*/
-func baseSearch(searchString string, fileExtensions []string, extendedSearch bool, forceStopChan chan bool) ([]string, bool) {
-	// check if the FileSystem is setup properly, if not reset it by regenerating it
-	if !cache.EntrieFilesystem.SetupProperly {
-		ForceUpdateCache()
+		sm.fileSystem = fs
+	} else if extended {
+		sm.fileSystem.Update(&sm.fileSystem.DefaultDirs, &sm.fileSystem.ExtendedDirs)
+		sm.fileSystem.Update(&sm.fileSystem.ExtendedDirs, &sm.fileSystem.DefaultDirs)
+	} else {
+		sm.fileSystem.Update(&sm.fileSystem.DefaultDirs, &sm.fileSystem.ExtendedDirs)
 	}
 
-	// make it so while we search we can't update the FileSystem
-	cache.EntrieFilesystem.Updateable = false
-	defer func() {
-		cache.EntrieFilesystem.Updateable = true
-	}()
-
-	// get the filepaths and names
-	results, pattern := search.Start(search.NewSearchString(searchString, fileExtensions), extendedSearch, forceStopChan)
-
-	// check if we have to stop the baseSearch
-	if len(forceStopChan) > 0 {
-		return []string{}, true
-	}
-
-	output := *search.Rank(results, pattern, forceStopChan)
-
-	// check if we have to stop the baseSearch
-	if len(forceStopChan) > 0 {
-		return []string{}, true
-	}
-
-	// rank and sort the files
-	return output, false
-}
-
-/*
-Search takes in any substring that you want to search for through all filenames. You may add any amount of file extensions as well.
-The extendedSearch flag dictates, if we search through the SecondaryDirs.
-To change the folders included/excluded in the search use the pkg/options set functions.
-
-On it's first execution the function will take longer, as it needs to generate the cache first.
-*/
-func Search(searchString string, fileExtensions []string, extendedSearch bool) []string {
-	results, _ := baseSearch(searchString, fileExtensions, extendedSearch, make(chan bool, 1)) // insert a dummy channel, as it's not needed here
-	return results
-}
-
-/*
-GoSearchWithBreak behaves exactly like Search, the only difference is, it requires a break channel as an input.
-
-This function should be started as a goroutine and it can be cancelled early by sending something in the breakChan.
-If it breaks early, it returns a true, otherwise false.
-*/
-func GoSearchWithBreak(searchString string, fileExtensions []string, extendedSearch bool, breakChan chan bool) ([]string, bool) {
-	forceStopChan := make(chan bool, 1)
-
-	go func() {
-		// check for when we receive the break signal
-		for range breakChan {
-			// send something into the foreStopChan to stop the baseSearch
-			forceStopChan <- true
-		}
-	}()
-
-	return baseSearch(searchString, fileExtensions, extendedSearch, forceStopChan)
-}
-
-/*
-ForceUpdateCache updates the cache regardless of it's state.
-
-This function is generally not needed. Though it can be useful, if you want to generate the cache early, before your first search.
-*/
-func ForceUpdateCache() {
-	cache.EntrieFilesystem = cache.New(config.BWSConfig.MainDirs, config.BWSConfig.SecondaryDirs)
 	runtime.GC()
+
+	return nil
 }
